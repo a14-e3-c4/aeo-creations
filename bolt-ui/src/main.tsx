2cbcc81aea1d3d0e1d3139bf3f148d7080172893
a14-e3-c4 <emmanuelawoniyi2010@gmail.com>
2026-09-05 20:31:21 +0100

    Expose free voice and avatar studio in the app

--- bolt-ui/src/main.tsx
+++ bolt-ui/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { FreeAvatarStudio } from './components/FreeAvatarStudio';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <FreeAvatarStudio />
   </StrictMode>
 );
